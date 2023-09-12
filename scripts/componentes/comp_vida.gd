e196752cb4a7c46ba101e02ec0989d56fb22d2e0
Alvaro Romeral <alrompin98@gmail.com>
2023-09-12 22:53:18 +0200

    Organizacion de ficheros

--- scripts/componentes/comp_vida.gd
+++ scripts/componentes/comp_vida.gd
@@ -1,2 +1,7 @@
 extends Node
 class_name CompVida
+
+@export var hp:float = 1
+
+func recibir_danno(cantidad:float):
+	hp -= cantidad
--- scripts/interfaces/pantalla_menu_principal.gd
+++ scripts/interfaces/pantalla_menu_principal.gd
@@ -1,23 +1,45 @@
 extends CanvasLayer
 class_name PantallaMenuPrincipal
 
-@export var boton_jugar:Button
+@export var boton_nueva_partida:Button
+@export var boton_cargar_partida:Button
 @export var boton_opciones:Button
+@export var boton_puntuacion:Button
 @export var boton_salir:Button
 
 func _ready():
-    boton_jugar.pressed.connect(_jugar)
-    boton_opciones.pressed.connect(_opciones)
-    boton_salir.pressed.connect(_salir)
+	if boton_nueva_partida:
+		boton_nueva_partida.pressed.connect(nueva_partida)
+	if boton_cargar_partida:
+		boton_cargar_partida.pressed.connect(cargar_partida)
+	if boton_opciones:
+		boton_opciones.pressed.connect(mostrar_opciones)
+	if boton_puntuacion:
+		boton_puntuacion.pressed.connect(mostrar_puntuaciones)
+	if boton_salir:
+		boton_salir.pressed.connect(salir_juego)
 
 
-func _jugar():
-    pass
+func nueva_partida():
+	get_tree().change_scene_to_file("res://scenes/Nivel_Temp.tscn")
 
 
-func _opciones():
-    pass
+func cargar_partida():
+	pass
 
 
-func _salir():
-    get_tree().quit()
+func mostrar_puntuaciones():
+	pass
+
+
+func mostrar_opciones():
+	pass
+
+
+func mostrar_creditos():
+	get_tree().change_scene_to_file("res://Pantallas/Menus/MenuCreditos.tscn")
+
+
+func salir_juego():
+	#guardar datos
+	get_tree().quit()
