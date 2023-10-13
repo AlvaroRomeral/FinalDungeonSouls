3e0384abaf343878f137ac6d9c4f95adf130b272
Alvaro Romeral <alrompin98@gmail.com>
2023-10-14 01:33:45 +0200

    Updated activador Oleadas

--- scripts/gameplay/activador_oleadas.gd
+++ scripts/gameplay/activador_oleadas.gd
@@ -5,9 +5,12 @@ class_name ActivadorOleadas
 
 @export var panel_zona_oleada:PanelZonaOleada
 @export var brasero:Brasero
+@export var area_detecccion:Area2D
 
 func _ready():
 	panel_zona_oleada.empezar_oleada_presionado.connect(llamar_a_comienza_oleadas)
+	area_detecccion.body_entered.connect(func(_cuerpo): panel_zona_oleada.show())
+	area_detecccion.body_exited.connect(func(_cuerpo): panel_zona_oleada.hide())
 
 
 func llamar_a_comienza_oleadas():
